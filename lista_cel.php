26e572a74e10de7786d5d104759309914c3cb259
iceseconds <85528521+iceseconds@users.noreply.github.com>
2021-06-25 22:34:27 -0300

    lista de celular

--- lista_cel.php
+++ lista_cel.php
@@ -3,114 +3,140 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Celular</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="css/layout.css">
+		<link rel="stylesheet" type="text/css" href="css/menu.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/grid.css">
+		<link href="https://fonts.googleapis.com/css?family=PT+Serif" rel="stylesheet">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
+	</head>
 
-<head>
-	<meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Celulares</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="css/layout.css">
-	<link rel="stylesheet" type="text/css" href="css/menu.css">
-	<link rel="stylesheet" type="text/css" href="css/grid.css">
-	<link href="https://fonts.googleapis.com/css?family=PT+Serif" rel="stylesheet">
-</head>
-
-<body>
-	<div class="principal">
-		<div class="topo topo_grid">
-			<div class="logo">
-				<h1> THE MAX Celulares </h1>
-				<h4> Controle de estoque e venda </h4>
+	<body>
+		<div class="principal">
+			<div class="topo topo_grid">
+				<div class="logo">
+					<h1> THE MAX Celulares </h1>
+					<h4> Controle de estoque e venda </h4>
+				</div>
+				<div class="menu_global">
+					<ul>
+						<li> Olá <?php include "valida_login.php"; ?> </li>
+						<li><a href="logout.php" class="active">Sair</a></li>
+					</ul>
+				</div>
 			</div>
-			<div class="menu_global">
-				<ul>
-					<li> Olá <?php include "valida_login.php"; ?> </li>
-					<li><a href="logout.php" class="active">Sair</a></li>
-				</ul>
+			<div class="menu_local">
+				<?php include "menu_local.php"; ?>
 			</div>
-		</div>
-		<div class="menu_local">
-			<?php include "menu_local.php"; ?>
-		</div>
-		<div class="conteudo_especifico">
-			<div class="centralizar">
-				<h1> CELULARES </h1>
-			</div>
-			<div id="funcionalidade" class="div_direita">
-				<?php
-				$conectar = mysqli_connect("localhost", "root", "", "themax");
-
-				$sql_consulta = "SELECT id_cel, marca_cel, nome_cel, memoria_cel, preco_cel FROM celular";
-				$resultado_consulta = mysqli_query($conectar, $sql_consulta);
-				?>
-				<p align="right"> <a href="cadastra_cel.php"> Cadastrar celular </a> </p>
-				<table class="centralizar">
-					<tr>
-						<td class="esquerda">
-							<p> Marca </p>
-						</td>
-						<td>
-							<p> Nome </p>
-						</td>
-						<td>
-							<p> Memoria </p>
-						</td>
-						<td>
-							<p> Preço </p>
-						</td>
-						<td class="direita">
-							<p> Ação </p>
-						</td>
-					</tr>
+			<div class="conteudo_especifico centralizar">
+				<div class="centralizar">
+					<h1> CELULARES </h1>
+				</div>
+				<div>
+					<a class="div_esquerda botao" href="cadastra_cel.php" title="Cadastrar celular">
+						Cadastrar celular <i class="fas fa-user-plus"></i>
+					</a>
+				</div>
+				<div id="funcionalidade" class="div_direita">
 					<?php
-					while ($registro = mysqli_fetch_row($resultado_consulta)) {
+					$conectar = mysqli_connect("localhost", "root", "", "themax");
+
+					$sql_consulta = "SELECT id_cel, marca_cel, nome_cel, memoria_cel, processador_cel, preco_cel FROM celular";
+					$resultado_consulta = mysqli_query($conectar, $sql_consulta);
 					?>
+					<table class="centralizar listaFun">
 						<tr>
-							<td class="esquerda">
-								<p>
-									<?php echo "$registro[1]"; ?>
-								</p>
+							<td class="esquerda" style="border-bottom: 1px solid black!important;">
+								<p> Marca </p>
 							</td>
-							<td>
-								<p>
-									<a href="exibe_cel.php?codigo=<?php echo $registro[0] ?>">
-										<?php echo "$registro[2]"; ?>
-									</a>
-								</p>
+							<td style="border-bottom: 1px solid black!important;">
+								<p> Nome </p>
 							</td>
-							<td>
-								<p>
-									<?php echo "$registro[3]"; ?>
-								</p>
+							<td style="border-bottom: 1px solid black!important;">
+								<p> Memoria </p>
 							</td>
-							<td>
-								<p>
-									<?php echo "$registro[4]"; ?>
-								</p>
+							<td style="border-bottom: 1px solid black!important;">
+								<p> Processador </p>
 							</td>
-							<td class="direira">
-								<p>
-									<a href="altera_cel.php?codigo=<?php echo $registro[0] ?>">
-										Alterar
-									</a>
-								</p>
+							<td style="border-bottom: 1px solid black!important;">
+								<p> Preço </p>
+							</td>
+							<td style="border-bottom: 1px solid black!important;">
+								<p> Ação </p>
 							</td>
 						</tr>
-					<?php
-					}
-					?>
-				</table>
+						<?php
+						while ($registro = mysqli_fetch_row($resultado_consulta)) {
+						?>
+							<tr>
+								<td class="esquerda">
+									<p>
+										<a class="exibe" href="exibe_cel.php?codigo=<?php echo $registro[0] ?>" title="Exibir celular">
+											<i class="fas fa-stream" style="color: white;"></i>
+										</a>&nbsp;
+										<?php echo "$registro[1]"; ?>
+									</p>
+								</td>
+								<td>
+									<p>
+										<?php echo "$registro[2]"; ?>
+									</p>
+								</td>
+								<td>
+									<p>
+										<?php echo "$registro[3]"; ?>
+									</p>
+								</td>
+								<td>
+									<p>
+										<?php echo "$registro[4]"; ?>
+									</p>
+								</td>
+								<td>
+									<p>
+										<?php echo "$registro[5]"; ?>
+									</p>
+								</td>
+								<td class="direita">
+									<p>
+										<a class="editar" href="altera_cel.php?codigo=<?php echo $registro[0] ?>" title="Editar celular">
+											<i class="fas fa-user-edit" style="color: white;"></i>
+										</a>
+										<a class="excluir" href="#abrirModal<?php echo $registro[0] ?>" data-confirm="Certeza?" title="Excluir celular">
+											<i class="fas fa-user-times" style="color: white;"></i>
+										</a>
+									</p>
+								</td>
+							</tr>
+							<div id="abrirModal<?php echo $registro[0] ?>" class="modal">
+								<div>
+									<a href="#fechar" title="Fechar" class="fechar">x</a>
+									<h2>Excluir celular</h2>
+									<p>Você realmente deseja excluir este celular?</p>
+									<a class="botaosim" href="processa_excluir_cel.php?codigo=<?php echo $registro[0] ?>">Sim</a>
+									<a class="botaonao" href="#fechar">Não</a>
+								</div>
+							</div>
+						<?php
+						}
+						?>
+					</table>
+				</div>
 			</div>
-		</div>
-		<div class="rodape">
-			<div id="texto_institucional">
+			<div class="rodape">
 				<div id="texto_institucional">
-					<h6> MAX - CONTROL </h6>
-					<h6> Rua do Technologia, 777 -- E-mail: contato@max_control.com.br -- Fone: (61) 99876 - 5432 </h6>
+					<div id="texto_institucional">
+						<h6> MAX - CONTROL </h6>
+						<h6> Rua do Technologia, 777 -- E-mail: contato@max_control.com.br -- Fone: (61) 99876 - 5432 </h6>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</body>
-
+	</body>
 </html>
