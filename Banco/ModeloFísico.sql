d96b99365f42b7fdcbb7ea627092d0894f1608d0
Pedro Paul <pedro.sales@cassi.com.br>
2021-07-03 23:52:31 -0300

    alteração script banco

--- Banco/ModeloFísico.sql
+++ Banco/ModeloFísico.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `themax`
 --
-CREATE DATABASE IF NOT EXISTS themax;
+DROP DATABASE IF EXISTS themax;
+CREATE DATABASE themax;
 -- --------------------------------------------------------
 
 --
